a10145732ad75e46e0cd6336eebd36b75c44307b
dhiraj.dhas <dhiraj.dhas@LAP-DHIRAJD.SUMASOFT-PDC>
2022-04-11 18:13:31 +0530

    test1 commit

--- db/conn.php
+++ db/conn.php
@@ -1,8 +1,17 @@
 <?php
-$host = 'localhost:3308';
-$db = 'attandance_db';
-$user = 'root';
-$pass ='';
+
+//localhost
+// $host = 'localhost:3308';
+// $db = 'attandance_db';
+// $user = 'root';
+// $pass ='';
+// $charset = 'utf8mb4_0900_ai_ci';
+
+//Remote Access
+$host = 'remotemysql.com';
+$db = '1iCvZO70qP';
+$user = '1iCvZO70qP';
+$pass ='vjHCm0PLq7';
 $charset = 'utf8mb4_0900_ai_ci';
 
 $dsn = "mysql:host=$host;dbname=$db;charse=$charset";
